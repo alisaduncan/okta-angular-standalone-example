7a19b42c50d24d005e83fe88a3fb19446a244e5f
Alisa <7586237+alisaduncan@users.noreply.github.com>
2024-01-29 14:31:20 -0800

    Add runtime config loading (#1)
    * demonstrate http config
    
    * missed file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,33 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { OktaAuthModule } from '@okta/okta-angular';
+import { OktaAuthConfigService, OktaAuthModule } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpBackend, HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
+import { tap, take } from 'rxjs';
 
-const oktaAuth = new OktaAuth({
-  issuer: 'https://{yourOktaDomain}/oauth2/default',
-  clientId: '{yourOktaClientID}',
-  redirectUri: window.location.origin + '/login/callback',
-});
+function configInitializer(httpBackend: HttpBackend, configService: OktaAuthConfigService): () => void {
+  return () =>
+  new HttpClient(httpBackend)
+  .get('api/config.json')
+  .pipe(
+    tap((authConfig: any) => configService.setConfig({oktaAuth: new OktaAuth({...authConfig, redirectUri: `${window.location.origin}/login/callback`})})),
+    take(1)
+  );
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
-      OktaAuthModule.forRoot({oktaAuth})
+      OktaAuthModule
     ),
     provideRouter(routes),
     provideHttpClient(withInterceptors([
       authInterceptor
-    ]))
+    ])),
+    { provide: APP_INITIALIZER, useFactory: configInitializer, deps: [HttpBackend, OktaAuthConfigService], multi: true },
   ]
 };
